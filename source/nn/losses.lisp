e375a3d301696c2387f3764bcae7d761a02abe72
hikettei <hikettei@gmail.com>
2022-12-30 13:19:58 +0900

    mse cross-entropy

--- source/nn/losses.lisp
+++ source/nn/losses.lisp
@@ -4,3 +4,8 @@
 (defun mse (p y)
   (mean (pow (sub p y) 2)))
 
+(defun cross-entropy (x y &optional (delta 1e-7))
+  (let* ((coeff (div -1.0 (max (length (data x)) 1)))
+	 (res (mul coeff (mul y (sum (log (add x delta)) 0))))) ; axis=0?
+    res))
+
