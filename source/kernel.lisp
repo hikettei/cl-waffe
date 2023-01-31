085223eb8f54281455cab49d66d12cabe39f5bd2
hikettei <hikettei@gmail.com>
2023-01-31 17:26:59 +0900

    with-kernel-case macro

--- source/kernel.lisp
+++ source/kernel.lisp
@@ -5,9 +5,11 @@
 
 ; dispaches kernel based on backends. and optimize node
 
-(defparameter *kernels* `(:mgl))
+(defparameter *kernels* `(:mgl)
+  "The list of cl-waffe supported kernels")
 
-(defparameter *destructive-operation* nil)
+(defparameter *destructive-operation* nil
+  "When t, some computations will be done destructively. Default is nil")
 
 (defmacro with-optimized-operation (&body body)
   ; doing all operations with destructive
@@ -93,3 +95,69 @@
   (setf (data target-tensor) (data (invoke-kernel kernel-function `(,target-tensor ,@args) target-data 0)))
   target-tensor)
 
+(defmacro with-kernel-case (target var &key (mgl nil) (mgl-cuda nil))
+  "Reading the target's device, this macro invokes property codes described in :mgl, :mgl-cuda etc...
+
+   Every time reaches this macro, cl-waffe caches the target (i.e. the target is allowed to be destructed).
+
+   This macro won't create computation nodes.
+
+   The available slot is in *kernels*
+
+   When :mgl-cuda is nil, automatically calls :mgl
+
+   This macro returns the last value of called slots.
+
+   The last value of :mgl, :mgl-cuda and so on, must be type of list (cons), or mgl-mat:mat, waffetensorcontenttype.
+
+   Note: the target's thread-data must be already created. (i.e. By the time tensors reach this macro, at least once they needed to be pathed through Trainer or Model.)
+   So, use this macro when you defining :forward and :backward in defnode macro because in defnode, backprop is disabled and computation nodes isn't always required.
+
+   Inputs: target, an target tensor.
+           var, where an copied tensor of target will be assigned.
+           :mgl mgl-mat, when using cpu.
+           :mgl-cuda mgl-mat, when using cuda.
+
+   Return: An tensor (where tensor is made by sysconst)
+
+   Example:
+   (with-kernel-case x o
+       :mgl (progn
+              (axpy! 1.0 a o)) ; axpy! = !add
+       :mgl-cuda nil) => #Const(((0.0 1.0 ~ 2.0 3.0)        
+                 ...
+        (0.0 4.0 ~ 5.0 6.0)) :mgl t :shape (10 10))
+
+   ; This is useful when defining :backward
+   (with-kernel-case x o
+       :mgl (progn
+              (list 1 1)))"
+  `(progn
+     (unless (typep ,target 'waffetensor)
+       (error "cl-waffe.with-kernel-case: target must be waffetensor. Encounted type of ~a, when using ~a" (type-of ,target) ,target))
+     (cl-waffe.caches:with-cache
+	 (,var ,target :place (cl-waffe.backends.mgl:create-thread-idx
+			       (waffetensor-thread-data ,target)))
+       (warranty ,target)
+       (labels ((mgl-cpu-step  (,var) ,@mgl)
+		(mgl-cuda-step (,var)
+		  (if (null `,mgl-cuda)
+		      (progn ,@mgl)
+		      (progn ,@mgl-cuda))))
+	 (let ((out (case (waffetensor-backend ,target)
+		      (:mgl (if (use-cuda-p (data ,target))
+					    (mgl-cuda-step ,var)
+					    (mgl-cpu-step ,var)))
+		      (T (error "cl-waffe.with-kernel-case: Encounted unsupported kernel ~a, cl-waffe supports kernel following: ~a"
+				(waffetensor-kernel ,target)
+				*kernels*)))))
+	   (typecase out
+	     (list
+	      (map 'list (lambda (x)
+			   (sysconst x :thread-data ,target))
+		   out))
+	     (T
+	      (sysconst
+	       out
+	       :thread-data
+	       (waffetensor-thread-data ,target)))))))))
