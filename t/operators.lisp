71b03bc287fa1aa9b4cdd9768b13cc3739d9576f
hikettei <hikettei@gmail.com>
2023-02-09 23:01:23 +0900

    test

--- t/operators.lisp
+++ t/operators.lisp
@@ -1,17 +1,6 @@
 
 (in-package :cl-waffe-test)
 
-; 基本関数のforward backwardを検証
-
 (in-suite :test)
 
-(setq a (parameter (randn 7 10)))
-(setq b (randn 7 10))
 
-(setq c (parameter (randn 8 3)))
-(print c)
-
-(test cl-waffe-test
-      (is (= 6 (mul 3 (add 1 1))))
-      (is (add a b))
-      (is (sub a b)))
---
+++ t/utils.lisp
@@ -0,0 +1,9 @@
+
+(in-package :cl-waffe-test)
+
+; utils for testing
+
+(defun ~= (x y)
+  )
+
+(defmacro with-operate (x &key mgl waffe))
