1d17444e979818552d6fed33563b4cbe02c65598
hikettei <hikettei@gmail.com>
2023-02-14 01:04:11 +0900

    deleted unused code

--- source/operators.lisp
+++ source/operators.lisp
@@ -950,26 +950,6 @@ acosh(x) = 1/cosh(x)"
 atanh(x) = 1/tanh(x)"
   (!div 1 (!tanh x)))
 
-(defmacro maxlist (list)
-  `(let ((max-item (apply #'max ,list)))
-     (if (<= max-item 1.0) 1.0 max-item)))
-
-(defun max-position-column (arr)
-  (declare (optimize (speed 3) (space 0) (safety 0) (debug 0))
-           (type (array single-float) arr))
-  (let ((max-arr (make-array (array-dimension arr 0)
-                             :element-type 'single-float
-                             :initial-element most-negative-single-float))
-        (pos-arr (make-array (array-dimension arr 0)
-                             :element-type 'fixnum
-                             :initial-element 0)))
-    (loop for i fixnum from 0 below (array-dimension arr 0) do
-      (loop for j fixnum from 0 below (array-dimension arr 1) do
-        (when (> (aref arr i j) (aref max-arr i))
-          (setf (aref max-arr i) (aref arr i j)
-                (aref pos-arr i) j))))
-    pos-arr))
-
 (defun !argmaxmin (tensor max-or-min &key (dim nil))
   "Todo: For GPU"
   (declare (optimize (speed 3))
