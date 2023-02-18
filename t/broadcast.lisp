a5b1fd4187c9ccdeb0d32b52a8db14c730adce80
hikettei <hikettei@gmail.com>
2023-02-18 22:14:28 +0900

    added test code

--- t/broadcast.lisp
+++ t/broadcast.lisp
@@ -3,7 +3,18 @@
 
 (in-suite :test)
 
-(setq a (!randn `(100 100)))
-(setq b (!randn `(100 1)))
+(format t "Testing for broadcasting shapes~%")
 
-(!add a b)
+(defparameter arg1 (!randn `(1000 1000)))
+(defparameter arg2 (!randn `(1000 1)))
+
+(defparameter arg3 (!randn `(1000 1)))
+
+(defun simple-test1 ()
+  (let* ((arg2-r (!repeats arg2 1 1000))
+	 (result (time (!add arg1 arg2)))
+	 (result1 (time (!add arg1 arg2-r))))
+    (M= (value result) (value result1))))
+    
+(test broadcasting-test
+      (is (simple-test1)))
