e2891a4615c282f26c4a534c59fa12444df24f9f
hikettei <hikettei@gmail.com>
2023-03-09 12:16:31 +0900

    moved sampling algos into distrubutions

---
+++ source/distributions/chisquare.lisp
@@ -0,0 +1,12 @@
+
+(in-package :cl-waffe)
+
+(defun !chisquare (dims df)
+  "@b(Not implemented yet)
+Todo: Use fast algorithms and approximations.
+
+Example:
+@begin[lang=lisp](code)
+@end[lang=lisp](code)"
+  (declare (ignore df dims))
+  (error "Not Implemented."))
