70d9a8c6b836344b807a23df7b485f4654be809a
hikettei <hikettei@gmail.com>
2023-03-09 14:15:40 +0900

    argmax tests

--- source/trainer.lisp
+++ source/trainer.lisp
@@ -271,26 +271,6 @@ Input: dataset ... dataset defined by defdataset.
     (setf (subseq base start-from (+ start-from (length title))) title)
     base))
 
-(defmacro maxlist (list)
-  `(let ((max-item (apply #'max ,list)))
-     (if (<= max-item 1.0) 1.0 max-item)))
-
-(defun max-position-column (arr)
-  (declare (optimize (speed 3) (space 0) (safety 0) (debug 0))
-           (type (array single-float) arr))
-  (let ((max-arr (make-array (array-dimension arr 0)
-                             :element-type 'single-float
-                             :initial-element most-negative-single-float))
-        (pos-arr (make-array (array-dimension arr 0)
-                             :element-type 'fixnum
-                             :initial-element 0)))
-    (loop for i fixnum from 0 below (array-dimension arr 0) do
-      (loop for j fixnum from 0 below (array-dimension arr 1) do
-        (when (> (aref arr i j) (aref max-arr i))
-          (setf (aref max-arr i) (aref arr i j)
-                (aref pos-arr i) j))))
-    pos-arr))
-
 (defun valid (trainer dataset batch-size)
   "Valid trainer"
   (let ((count 0)
