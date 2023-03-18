a3986d973ad875c8562f029b9060e9a3e118d0ed
hikettei <hikettei@gmail.com>
2023-03-18 10:13:57 +0900

    exported attns

--- source/nn/package.lisp
+++ source/nn/package.lisp
@@ -4,6 +4,9 @@
 (defpackage cl-waffe.nn
   (:use :cl :cl-waffe)
   (:documentation "An packages for nn utils.")
+  (:export
+   :ScaleDotProductAttention
+   :MultiHeadAttention)
   (:export :linear
            :linearlayer
 	   :denselayer
