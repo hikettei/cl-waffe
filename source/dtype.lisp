65475d6f2d78616de2f167ce15d1de59670763b8
hikettei <hikettei@gmail.com>
2023-03-23 18:08:20 +0900

    added: with-dtype

--- source/dtype.lisp
+++ source/dtype.lisp
@@ -1,3 +1,15 @@
 
 (in-package :cl-waffe)
 
+(defparameter *dtypes* `(:float)) ; To Add: :half
+
+(defun dtype-p (dtype)
+  (if (find dtype *dtypes*)
+      dtype
+      (error "Invaild dtype ~a. Dtype must be chosen by following: ~a"
+	     dtype
+	     *dtypes*)))
+
+(defmacro with-dtype (dtype &body body)
+  `(let ((mgl-mat:*DEFAULT-MAT-CTYPE* ,(dtype-p dtype)))
+     ,@body))
