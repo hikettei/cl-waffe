c53f51630239a7521f90c45f6b8e90e8837c1e22
hikettei <hikettei@gmail.com>
2023-03-25 00:14:50 +0900

    benchmark of broadcasting

--- benchmark/benchmark1.lisp
+++ benchmark/benchmark1.lisp
@@ -12,7 +12,7 @@ export OPENBLAS_NUM_THREADS=2
 (defparameter *N* 100 "Trial N")
 (defparameter *backend-name* "OpenBLAS")
 
-(defparameter *MATMUL_SIZE* `(16 32 64 256 512 1024)); 2048)); 4096 8192))
+(defparameter *MATMUL_SIZE* `(16 32 64 256 512 1024 2048)); 4096 8192))
 
 (defparameter *BROADCAST_SHAPE*
   `(((10 10 1) (1 10 10))
@@ -75,32 +75,33 @@ export OPENBLAS_NUM_THREADS=2
 (defparameter *SLICE_SAVE_DIR* "./benchmark/results/SLICE_waffe.png")
 
 (defun compare-to-python ()
-  (format t "LLA Config:~%~a~%~%" cl-user::*lla-configuration*)
-  (format t "ℹ️ Running matmul_2D...~%~%")
+  (with-no-grad
+    (format t "LLA Config:~%~a~%~%" cl-user::*lla-configuration*)
+    (format t "ℹ️ Running matmul_2D...~%~%")
 
-  (let ((result (loop for i fixnum upfrom 0 below (length *MATMUL_SIZE*)
-		      collect (matmul_2d (nth i *MATMUL_SIZE*)))))
-    (plot (map 'list #'(lambda (x) (coerce x 'double-float)) result)
-      :x-seq *MATMUL_SIZE*
-      :title (format nil "matmul (cl-waffe + ~a) N=~a" *backend-name* *N*)
-      :x-label "Matrix Size"
-      :y-label "time (second)"
-      :output *MATMUL_SAVE_DIR*
-      :output-format :png)
-    ; To Add: output-to-csv
-    (format t "⭕️ The result is correctly saved at ~a~%~%" *MATMUL_SAVE_DIR*))
+    (let ((result (loop for i fixnum upfrom 0 below (length *MATMUL_SIZE*)
+			collect (matmul_2d (nth i *MATMUL_SIZE*)))))
+      (plot (map 'list #'(lambda (x) (coerce x 'double-float)) result)
+	:x-seq *MATMUL_SIZE*
+	:title (format nil "matmul (cl-waffe + ~a) N=~a" *backend-name* *N*)
+	:x-label "Matrix Size"
+	:y-label "time (second)"
+	:output *MATMUL_SAVE_DIR*
+	:output-format :png)
+      ; To Add: output-to-csv
+      (format t "⭕️ The result is correctly saved at ~a~%~%" *MATMUL_SAVE_DIR*))
 
-  (format t "ℹ️ Running broadcasting...~%~%")
-  (let ((result (loop for i fixnum upfrom 0 below (length *BROADCAST_SHAPE*)
-		      collect (broadcast-bench (nth i *BROADCAST_SHAPE*)))))
+    (format t "ℹ️ Running broadcasting...~%~%")
+    (let ((result (loop for i fixnum upfrom 0 below (length *BROADCAST_SHAPE*)
+			collect (broadcast-bench (nth i *BROADCAST_SHAPE*)))))
 
-    (plot (map 'list #'(lambda (x) (coerce x 'double-float)) result)
-      :title (format nil "broadcasting (cl-waffe + ~a) N=~a"
-		     *backend-name*
-		     *N*)
-      :x-seq (map 'list #'(lambda (x) (second (car x))) *BROADCAST_SHAPE*)
-      :x-label "Matrix Size"
-      :y-label "time (second)"
-      :output *BROADCASTING_SAVE_DIR*
-      :output-format :png)
-    (format t "⭕️ The result is correctly saved at ~a~%~%" *BROADCASTING_SAVE_DIR*)))
+      (plot (map 'list #'(lambda (x) (coerce x 'double-float)) result)
+	:title (format nil "broadcasting (cl-waffe + ~a) N=~a"
+		       *backend-name*
+		       *N*)
+	:x-seq (map 'list #'(lambda (x) (second (car x))) *BROADCAST_SHAPE*)
+	:x-label "Matrix Size"
+	:y-label "time (second)"
+	:output *BROADCASTING_SAVE_DIR*
+	:output-format :png)
+      (format t "⭕️ The result is correctly saved at ~a~%~%" *BROADCASTING_SAVE_DIR*))))
--- benchmark/numpy_benchmark.py
+++ benchmark/numpy_benchmark.py
@@ -20,13 +20,18 @@ print(np.show_config())
 BACKEND_NAME = "MKL"
 N = 100
 MATMUL_SIZE = [16, 32, 64, 256, 512, 1024, 2048]#, 4096, 8192]
-
+BROADCASTING_SHAPE = [[[10, 10, 1], [1, 10, 10]],
+                      [[100, 100, 1], [1, 100, 100]],
+                      [[200, 200, 1], [1, 200, 100]],
+                      [[300, 300, 1], [1, 300, 100]]]
 
 # Files
 MATMUL_RESULT_DIR = './benchmark/results/matmul_numpy.png'
+BROADCASTING_RESULT_DIR = './benchmark/results/broadcasting_numpy.png'
 
 # Counters
 matmul_try_n = 1
+broadcasting_try_n = 1
 
 def matmul_2D(K=1000):
     global matmul_try_n
@@ -39,6 +44,21 @@ def matmul_2D(K=1000):
         t2 = time()
         return t2 - t1
     return [run_test() for i in range(N)]
+
+def broadcasting_2D(K):
+    global broadcasting_try_n
+    print(f"[{broadcasting_try_n}/{len(BROADCASTING_SHAPE)}] Testing on {K[0][1]}*{K[0][1]} Matrix for {N} times...")
+    broadcasting_try_n += 1
+    a = np.random.randn(K[0][0], K[0][1], K[0][2])
+    b = np.random.randn(K[1][0], K[1][1], K[1][2])
+
+    def run_test():
+        t1 = time()
+        np.add(a, b)
+        t2 = time()
+        return t2 - t1
+    return [run_test() for i in range(N)]
+    
 
 
 if __name__ == "__main__":
@@ -55,5 +75,17 @@ if __name__ == "__main__":
     plt.savefig(MATMUL_RESULT_DIR)
     # Todo Output to CSV and merge graphs
     print(f"⭕️ The result is correctly saved at {MATMUL_RESULT_DIR}")
-    print("Running broadcasting 4D...")
-    
+    print("Running broadcasting...")
+    print("")
+    plt.cla()
+    broadcasting_result = []
+    for case in BROADCASTING_SHAPE:
+        result = broadcasting_2D(case)
+        broadcasting_result.append(mean(result))
+    plt.plot([K[0][1] for K in BROADCASTING_SHAPE], broadcasting_result)
+    plt.title(f"broadcasting (numpy + {BACKEND_NAME}) (N={N})")
+    plt.xlabel("Matrix Size")
+    plt.ylabel("time (second)")
+    plt.savefig(BROADCASTING_RESULT_DIR)
+    # Todo Output to CSV and merge graphs
+    print(f"⭕️ The result is correctly saved at {BROADCASTING_RESULT_DIR}")
