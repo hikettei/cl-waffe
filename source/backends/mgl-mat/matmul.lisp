663c6b5d39942e399c659c9ec7e4e060b4573bfe
hikettei <hikettei@gmail.com>
2023-03-24 17:32:35 +0900

    optim: matmul

--- source/backends/mgl-mat/matmul.lisp
+++ source/backends/mgl-mat/matmul.lisp
@@ -4,38 +4,35 @@
 (deftype batch-size ()
   `(integer 0 100000000))
 
-(defun %matmul ())
-
-; %pmatmul <- with-thread-cached-matで並列化
-
 (declaim (ftype (function (boolean waffetensor waffetensor waffetensor &optional (or null mat) boolean boolean) mgl-mat:mat) matmul-tensor))
 ; Note: matmul would return unexcepted value if x or y is displaced.
 ; To prevent this, we probably need to create copy in advance.
+
 (defun matmul-tensor (enable-optimize? o x y
 		      &optional (output-to nil) (trans-a? nil) (trans-b? nil))
-  (declare (optimize (speed 3))
+  (declare (optimize (speed 3) (safety 0))
 	   (ignore o)
 	   (type boolean enable-optimize?)
-	   (type waffetensor o x y))
+	   (type waffetensor o x y)
+	   (ignore enable-optimize?))
   (let* ((transpose-map `(,(or trans-a? (is-transpose? x))
 			  ,(or trans-b? (is-transpose? y))))
 	 (x1 (value x :ignore-transpose t))
 	 (y1 (value y :ignore-transpose t)))
     (declare (type mat x1 y1))
-
     (unless (or (<= (length (the list (mat-dimensions x1))) 3)
 		(<= (length (the list (mat-dimensions y1))) 3))
       (error "cl-waffe.backends.mgl:matmul-tensor Matmul only supports following: 2d * 2d, 2d * 3d, 3d * 2d, 3d * 3d."))
 
-    ; x and y is a mat which isn't trasposed even when transpose is called befrore.
-    ; transpose-mat indicates x or y called trasnposed before calling matmul. 
+					; x and y is a mat which isn't trasposed even when transpose is called befrore.
+					; transpose-mat indicates x or y called trasnposed before calling matmul. 
     (let ((x-dims (the list (mat-dimensions x1)))
 	  (y-dims (the list (mat-dimensions y1))))
       (cond
 	((and (= (length x-dims) 2)
 	      (= (length y-dims) 2))
 
-	 ; Todo: check shapes
+					; Todo: check shapes
 
 	 (let ((out-dim `(,(if (car transpose-map)
 			       (car (reverse (mat-dimensions x1)))
@@ -53,6 +50,7 @@
 	     out)))
 	((and (= (length x-dims) 3)
 	      (= (length y-dims) 2))
+	 "A[0] * B[0], A[0] * B[1] ..."
 	 (let ((out-dim `(,(car x-dims)
 			  ,(if (car transpose-map)
 			       (nth 2 x-dims)
@@ -64,7 +62,7 @@
 	       (shape-first    (mat-dimensions x1)))
 	   (let ((out (or output-to (make-mat out-dim))))
 	     (dotimes (i (the fixnum (car x-dims)))
-	       (declare (type batch-size i)) ; note here!!!
+	       (declare (type batch-size i))
 	       (reshape-and-displace! out
 				      (cdr out-dim)
 				      (the fixnum (* i
@@ -77,7 +75,12 @@
 		(the fixnum (* i
 			       (the batch-size (nth 1 shape-first))
 			       (the batch-size (nth 2 shape-first)))))
-	       (matmul-tensor-2d out x1 y1 (car transpose-map) (second transpose-map)))
+	       (matmul-tensor-2d
+		out
+		x1
+		y1
+		(car transpose-map)
+		(second transpose-map)))
 	     (reshape-and-displace! out out-dim 0)
 	     (reshape-and-displace! x1 shape-first displace-first)
 	     out)))
@@ -117,9 +120,8 @@
 	     (reshape-and-displace! out out-dim 0)
 	     (reshape-and-displace! y1 shape-first displace-first)
 	     out)))
-	((= (length x-dims) (length y-dims))
+	((= (length x-dims) (length y-dims) 3)
 	 ; Otherwise, Batch Filter is adapted
-
 	 (let* ((dims (1- (length x-dims)))
 		(batch-dims
 		  (loop for i fixnum upfrom 0 below (- dims 1)
@@ -134,59 +136,55 @@
 		    ,(if (second transpose-map)
 			 (nth (1- dims) y-dims)
 			 (nth dims y-dims))))
-		(out (or output-to (make-mat `(,@batch-dims ,@output-tmp-dim))))
+		(out (or output-to
+			 (make-mat `(,@batch-dims ,@output-tmp-dim))))
 		(displace-first1 (mat-displacement x1))
 		(displace-first2 (mat-displacement y1))
 		(out-dim (mat-dimensions out))
 		(shape-first1 (mat-dimensions x1))
-		(shape-first2 (mat-dimensions y1)))
-	   (dotimes (i (the fixnum (car batch-dims)))
+		(shape-first2 (mat-dimensions y1))
+		(strides-out (the fixnum
+				  (* (the batch-size (nth 1 out-dim))
+				     (the batch-size (nth 2 out-dim)))))
+		(strides-1   (the fixnum (* (the batch-size (nth 1 shape-first1))
+					    (the batch-size (nth 2 shape-first2)))))
+		(strides-2   (the fixnum (* (the batch-size (nth 1 shape-first2))
+					    (the batch-size (nth 2 shape-first2)))))
+		(out-rest (cdr out-dim))
+		(rest-1 (cdr shape-first1))
+		(rest-2 (cdr shape-first2)))
+	   
+	   (dotimes (i (the batch-size (car batch-dims)))
 	     (declare (type batch-size i))
 	     (reshape-and-displace! ;e.g.: (n 3 5) => k + (3 5) + m, k+m=n
 	      out
-	      (cdr out-dim)
+	      out-rest
 	      (the fixnum
-		   (* i
-		      (the batch-size
-			   (nth 1 out-dim))
-	              (the batch-size
-			   (nth 2 out-dim)))))
-	     
+		   (* i strides-out)))
 	     (reshape-and-displace! x1
-				    (cdr shape-first1)
+				    rest-1
 				    (the fixnum
-					 (* i
-					    (the batch-size
-						 (nth 1 shape-first1))
-					    (the batch-size
-						 (nth 2 shape-first1)))))
-	     
+					 (* i strides-1)))
 	     (reshape-and-displace! y1
-				    (cdr shape-first2)
+				    rest-2
 				    (the fixnum
-					 (* i
-					    (the batch-size
-						 (nth 1 shape-first2))
-					    (the batch-size
-						 (nth 2 shape-first2)))))
-
+					 (* i strides-2)))
 	     ; displace tensors (i.e: make it 2d and 2d) and apply matmul.
+	     (matmul-tensor-2d
+	      out
+	      x1
+	      y1
+	      (car transpose-map)
+	      (second transpose-map)))
 
-	     (matmul-tensor enable-optimize?
-			    x
-			    (const x1)
-			    (const y1)
-			    out
-			    (car transpose-map)
-			    (second transpose-map))
-	     ; reset displace
-	     (reshape-and-displace! x1 shape-first1 displace-first1)
-	     (reshape-and-displace! y1 shape-first2 displace-first2))
-
+	   ; reset displace
+	   (reshape-and-displace! x1 shape-first1 displace-first1)
+	   (reshape-and-displace! y1 shape-first2 displace-first2)
 	   (reshape-and-displace!
 	    out
 	    out-dim
 	    0)
+	   
 	   out))
 	(T (error "cl-waffe.backends.mgl:matmul-tensor Operands could not broadcasted together. Can't multiply ~a and ~a. These tensors' dims must be <= 3" x y))))))
 
