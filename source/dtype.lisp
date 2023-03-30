25b1f53d162088a697f2c749a1fcc9dae7f322b5
hikettei <hikettei@gmail.com>
2023-03-30 19:38:28 +0900

    added: define-with-dtype

--- source/dtype.lisp
+++ source/dtype.lisp
@@ -1,7 +1,9 @@
 
 (in-package :cl-waffe)
 
-(defparameter *dtypes* `(:float)) ; To Add: :half
+(defparameter *dtypes* `(:float :double)) ; To Add: :half
+(defparameter *dtype-prefixes* `(-f -d))
+(defparameter *dtype-cl-names* `(single-float double-float))
 
 (defun dtype-p (dtype)
   (if (find dtype *dtypes*)
@@ -14,3 +16,56 @@
   `(let ((mgl-mat:*DEFAULT-MAT-CTYPE* ,(dtype-p dtype)))
      ,@body))
 
+(defun map-tree (fn tree)
+  (let ((tree (funcall fn tree)))
+    (if (listp tree)
+        (mapcar (lambda (subtree)
+                  (map-tree fn subtree))
+                tree)
+        tree)))
+
+(defun replace-lisp-code-with-dtype (body type-var dtype)
+  (map-tree #'(lambda (code)
+		(typecase code
+		  (symbol
+		   (if (equal (symbol-name code)
+			      (symbol-name type-var))
+		       dtype
+		       code))
+		  (T
+		   code)))
+	    body))
+
+(defmacro define-with-typevar
+    (function-name
+     type-specifier
+     (&rest args)
+     &body body
+     &aux (fnames (map
+		   'list
+		   #'(lambda (p)
+		       (symb function-name p))
+		   *dtype-prefixes*)))
+  (labels ((define-lisp-code-with (fname-with-prefix dtype)
+	     `(defun ,fname-with-prefix (,@args)
+		,@(replace-lisp-code-with-dtype
+		   body
+		   type-specifier
+		   dtype))))
+    `(progn
+       ,@(loop for i fixnum upfrom 0 below (length fnames)
+	       collect (define-lisp-code-with
+			   (nth i fnames)
+			   (nth i *dtype-cl-names*)))
+       (defun ,function-name (,@args)
+	 (declare (optimize (speed 3) (safety 0))
+		  (inline ,@fnames))
+	 (case mgl-mat:*DEFAULT-MAT-CTYPE*
+	   (:half
+	    (error "No implementation"))
+	   (:float
+	    (,(car fnames) ,@args))
+	   (:double
+	    (,(second fnames) ,@args))
+	   (T
+	    (error "No dtype")))))))
