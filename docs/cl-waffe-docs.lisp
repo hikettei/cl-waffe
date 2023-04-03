3e81aa811d357e51c5074a55f0df75f4c5c71072
hikettei <hikettei@gmail.com>
2023-04-03 22:20:50 +0900

    added: sections

--- docs/cl-waffe-docs.lisp
+++ docs/cl-waffe-docs.lisp
@@ -2,11 +2,37 @@
 (in-package :cl-waffe.documents)
 
 (with-page *cl-waffe-doc* "Package :cl-waffe"
-  )
+  (macrolet ((redirect (name &optional (comment ""))
+	       `(insert (format nil "@link[uri=\"./<apis--cl-waffe>.html#~a\"](~a) ~a~%~%" ,name ,name ,comment)))
+	     (with-section1 (title comment &body body)
+	       `(with-section ,title
+		  (insert "~%@begin(deflist)~%@def(~a)~%@begin(term)~%" ,comment)
+		  ,@body
+		  (insert "~%@end(term)~%@end(deflist)~%"))))
+
+    (with-section1 "Sampling Distributions" "Initializes the tensor of the given dim with specified algorithms."
+      (redirect "!normal")
+      (redirect "!randn")
+      (redirect "!uniform-random")
+      (redirect "!beta")
+      (redirect "!gamma")
+      (redirect "!chisquare")
+      (redirect "!bernoulli")
+      (redirect "!binomial"))
+
+    (with-section1 "Tensor Initializer" ""
+      (redirect "!zeros")
+      (redirect "!ones")
+      (redirect "!fill")
+      (redirect "!zeros-like")
+      (redirect "!ones-like")
+      (redirect "!full-like"))
+      
+    ))
 
 (with-page *APIS-cl-waffe* "<APIs: cl-waffe>"
 
-  ; distributions
+  ; Initializers/distributions of tensor.
   (with-api "function" "!normal"
     (with-evals "(!normal `(10 10)) :mean 2.0 :stddev 1.0"))
 
@@ -41,12 +67,18 @@
       "(!random `(10 10) `(1.0 2.0))"
       "(!random `(10 10) `(1 5))"))
 
-  (with-api "function" "!shape")
-  (with-api "function" "!dims")
-  (with-api "function" "!size")
   (with-api "function" "!zeros-like")
   (with-api "function" "!ones-like")
   (with-api "function" "!full-like")
+
+  (with-api "function" "!zeros")
+  (with-api "function" "!ones")
+  (with-api "function" "!fill")
+
+  ; accessors
+  (with-api "function" "!shape")
+  (with-api "function" "!dims")
+  (with-api "function" "!size")
   
 
 
